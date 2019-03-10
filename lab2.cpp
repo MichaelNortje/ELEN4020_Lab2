b58c8783dc4e37c024eb9cbe1efaafcd15d0db2e
Tyson <tyson.cross@gmail.com>
2019-03-10 15:51:23 +0200

    OpenMP Diagonal threaded algorithm added

--- lab2.cpp
+++ lab2.cpp
@@ -31,7 +31,7 @@ shared_ptr<vector<shared_ptr<vector<uint32_t>>>> generate2d(int N){
 void print2d(shared_ptr<vector<shared_ptr<vector<uint32_t>>>> A){
     const auto N = A->size();
     const auto M = A->at(0)->size();
-    cout << N << "x" << "M" << endl;
+    cout << N << "x" << M << endl;
     for (auto i = 0; i < N; i++){
         for (auto j = 0; j < M; j++){
             cout << A->at(i)->at(j) << "\t";
@@ -44,7 +44,9 @@ void print2d(shared_ptr<vector<shared_ptr<vector<uint32_t>>>> A){
 void transposeMatrixSerial(shared_ptr<vector<shared_ptr<vector<uint32_t>>>> A, int N){
     for (auto i = 0; i < N; i++){
         for (auto j = 0; j < i; j++){
-            swap(A->at(j)->at(i), A->at(i)->at(j)); // Why reinvent the wheel?
+            if (i!=j) {
+                swap(A->at(j)->at(i), A->at(i)->at(j)); // Don't reinvent the wheel
+            }
         }
     }
 }
@@ -55,9 +57,37 @@ void transposeMatrixSimpleOpenMP(shared_ptr<vector<shared_ptr<vector<uint32_t>>>
     for (auto i = 0; i < N; i++){
         #pragma omp parallel for
         for (auto j = 0; j < i; j++){
-            swap(A->at(j)->at(i), A->at(i)->at(j));
+            if (i!=j) {
+                swap(A->at(j)->at(i), A->at(i)->at(j));
+            }
         }
     }
+}
+
+/// \brief Transposes a matrix, using OpenMP threaded algorithm, swapping row and columns
+void transposeMatrixDiagonalOpenMP(shared_ptr<vector<shared_ptr<vector<uint32_t>>>> A, int N){
+    int i, j, tid;
+    int num_threads = omp_get_num_threads();
+
+    #pragma omp parallel shared(A, N, num_threads) private(tid, i, j)
+    {
+        // if (num_threads > N) {
+        //     omp_set_num_threads(N);
+        //     num_threads = N;  
+        // }
+        tid = omp_get_thread_num();
+        auto start_thread_num = tid/num_threads;
+        // printf("Number of threads: %d \n", num_threads);
+        // printf("TiD: %2d, from %d to %d \n", tid, start_thread_num, N);
+        for (i = start_thread_num; i < N; i++){
+                for(j = start_thread_num; i < N; i++) {
+                    if (i!=j) {
+                            // printf("Thread %d did (%d,%d)->(%d,%d) \n%", tid, j, i, i, j);
+                            swap(A->at(j)->at(i), A->at(i)->at(j));
+                        }
+                }
+        }
+    };
 }
 
 ////////////////////////////
@@ -78,38 +108,47 @@ int main(){
 	output_file << setw(width) << left << "Serial";
 	output_file << setw(width) << left << "OpenMP:Naive";
     output_file << setw(width) << left << "OpenMP:Diagonal";
-	output_file << setw(width) << left << "OpenMP:Blocked";
+	output_file << setw(width) << left << "OpenMP:Block-Oriented";
     output_file << setw(width) << left << "PThreads:Diagonal";
-	output_file << setw(width) << left << "PThreads:Blocked";
-
+	output_file << setw(width) << left << "PThreads:Block-Oriented";
 
 	output_file << endl;
+    
+    auto print = false;
 
     vector<int> sizes = {128, 1024, 2048, 4096, 8192};
-    // vector<int> sizes = {2, 4, 6, 8};
+    // vector<int> sizes = {3, 5, 6, 8};
+    // vector<int> sizes = {8};
 
     for (auto& N : sizes)
     {
         shared_ptr<vector<shared_ptr<vector<uint32_t>>>> A = generate2d(N);
-        // print2d(A);
+        if (print) print2d(A);
 
         steady_clock::time_point t1 = steady_clock::now();
         transposeMatrixSerial(A, N);
         steady_clock::time_point t2 = steady_clock::now();
         auto timer_serial = duration_cast<duration<double>>(t2 - t1);
-        // print2d(A);
+        if (print) print2d(A);
 
         t1 = steady_clock::now();
         transposeMatrixSimpleOpenMP(A, N);
         t2 = steady_clock::now();
         auto timer_simple_OpenMP = duration_cast<duration<double>>(t2 - t1);
-        // print2d(A);
+        if (print) print2d(A);
+
+        t1 = steady_clock::now();
+        transposeMatrixDiagonalOpenMP(A, N);
+        t2 = steady_clock::now();
+        auto timer_diagonal_OpenMP = duration_cast<duration<double>>(t2 - t1);
+        if (print) print2d(A);
 
         /* Output Time */
 		output_file << setw(width) << left << N;
 		output_file << setw(width) << left << setprecision(7) << fixed << timer_serial.count();
 		output_file << setw(width) << left << setprecision(7) << fixed << timer_simple_OpenMP.count();
-		output_file << endl;
+        output_file << setw(width) << left << setprecision(7) << fixed << timer_diagonal_OpenMP.count();
+        output_file << endl;
     }
 
 	output_file.close();
